77cf606fa9b72476df4b4103e725e253ec37ad27
macielthiago <maciel.rosari54@gmail.com>
2023-10-10 09:20:50 -0400

    Exercícios de ficação

---
+++ Laboratório/Lab-01/Fixação-txt/Origem-TXT.cpp
@@ -0,0 +1,57 @@
+// exercícios de fixação do laboratório 1 - formato texto
+#include <iostream>
+using namespace std;
+
+int main()
+{
+/* ==================================================================================================================
+	
+	1) Descreva passo a passo como criar um projeto em C++ no Visual Studio.
+	* Abra o Visual Studio:
+		> Criar um projeto > Projeto vazio > Nome do projeto > Próximo > Arquivos de origem > Adicionar > Novo item
+		  Nome > adicionar
+
+	-----------------------------------------------------------------------------------------------------------------
+
+	2) Descreva como fechar um projeto aberto sem fechar o Visual Studio.
+	  
+	   Arquivos de origem > "nome do projeto" > com o botão direito > Excluir do projeto
+
+	-----------------------------------------------------------------------------------------------------------------
+
+	3) Descreva os passos necessários para apagar por completo um projeto do disco, 
+	   sem deixar rastros dele no Visual Studio.
+
+	* Arquivos de origem > "nome do projeto" > com o botão direito > remover
+
+	-----------------------------------------------------------------------------------------------------------------
+
+	4) Onde está localizado "primeiro.cpp"? Indique o caminho completo.
+
+	* Documentos > Estudos > ufersa > progcomp > aulas > aula-01
+
+	-----------------------------------------------------------------------------------------------------------------
+
+	5) Um programa com um erro semântico pode executar, mas não dá a resposta esperada. 
+	   Que tipo de ferramentas o Visual Studio oferece para tratar erros em um programa?
+
+	* Usando o depurador do Visual Studio para verificar linha a inha onde está o erro. 
+
+	-----------------------------------------------------------------------------------------------------------------
+
+	6) Qual opção de menu, e a tecla de atalho correspondente, para iniciar a depuração de um programa no Visual Studio?
+
+	* Menu > Depurar > Iniciar depuração.  F5
+	
+	-----------------------------------------------------------------------------------------------------------------
+
+	7) O que é um ponto de parada (breakpoint) e como adicionar um no Visual Studio?
+
+	* Os breakpoints são lugares (linhas ou funções) onde você quer que o programa pare a execução e 
+	  espere por um comando seu (para imprimir variáveis, para executar uma linha por vez, etc).
+
+
+
+
+*/
+}
