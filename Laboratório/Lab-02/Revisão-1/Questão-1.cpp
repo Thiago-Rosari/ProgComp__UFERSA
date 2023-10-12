1f2fd8c26117a8238e4e6bb9c76c11ef6936417a
macielthiago <maciel.rosari54@gmail.com>
2023-10-12 09:47:57 -0400

    Exercícios de revisão

---
+++ Laboratório/Lab-02/Revisão-1/Questão-1.cpp
@@ -0,0 +1,29 @@
+//questão 1 de revisão
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1252);
+	SetConsoleOutputCP(1252);
+
+	cout << "Bem vindo" << endl << "ao mundo" << endl << "do C++";
+	cout << endl << endl;
+
+// questão 2 de revisão
+
+	cout << "Bem vindo " << endl;
+	cout << "ao mundo " << endl;
+	cout << "do C++" << endl;
+	cout << endl;
+
+	cout << "Bem vindo ";
+	cout << "ao mundo ";
+	cout << "do C++";
+	cout << endl;
+
+
+
+	return 0;
+}
---
+++ Laboratório/Lab-02/Revisão-3/Questão-3.cpp
@@ -0,0 +1,19 @@
+// questão de revisão 3
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	// exibindo uma frase que contém acentos
+	cout << "Acentuação é possível?" << endl;
+
+	// leia uma palavra que contém acentos
+	char palavra[40];
+	cin >> palavra;
+
+	// exibindo o que foi lido
+	cout << palavra << endl;
+
+	return 0;
+}
