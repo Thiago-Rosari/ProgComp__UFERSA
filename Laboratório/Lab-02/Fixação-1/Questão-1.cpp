961158ba8e6be5f2ef24a161a2e0e6f9b470d849
macielthiago <maciel.rosari54@gmail.com>
2023-10-13 09:29:30 -0400

    Resolução dos exercícios de fixação

---
+++ Laboratório/Lab-02/Fixação-1/Questão-1.cpp
@@ -0,0 +1,16 @@
+// exercício de fixação 1
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1252);
+	SetConsoleOutputCP(1252);
+
+	cout << "Antônio Bezerra" << endl;
+	cout << "Av. Costa Larga, 540" << endl;
+	cout << "Mossoró, RN" << endl;
+
+	return 0;
+}
---
+++ Laboratório/Lab-02/Fixação-2/Questão-2.cpp
@@ -0,0 +1,17 @@
+// exercício de fixação 2
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1252);
+	SetConsoleOutputCP(1252);
+
+	cout << "Antônio Bezerra" << endl;
+	cout << "Antônio " << endl << "Bezerra" << endl;
+	cout << "Antônio ";
+	cout << "bezerra" << endl;
+
+	return 0;
+}
