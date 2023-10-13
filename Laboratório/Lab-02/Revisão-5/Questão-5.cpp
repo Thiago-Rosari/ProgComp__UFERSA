2711c75d4bf0f416f7c8ab8b7966be7f4ef1b70d
macielthiago <maciel.rosari54@gmail.com>
2023-10-13 09:08:35 -0400

    Exercício 5 - argumentos

---
+++ Laboratório/Lab-02/Revisão-5/Questão-5.cpp
@@ -0,0 +1,10 @@
+// exercício de revisão 5
+#include <iostream>
+using namespace std;
+
+int main(int argc, char ** argv)
+{
+	cout << argv[1] << " " << argv[2] << " programar!" << endl;
+
+	return 0;
+}
