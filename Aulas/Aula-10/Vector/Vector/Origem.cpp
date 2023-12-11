3eb7d7229a36c083c6bc70eba849d51b9cbdc3ca
macielthiago <maciel.rosari54@gmail.com>
2023-12-11 15:22:35 -0400

    Vetores e funções

---
+++ Aulas/Aula-10/Vector/Vector/Origem.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main()
+{
+	vector<int> vetI;   // cria vetor vazio de itens
+	int n;
+	cin >> n;
+	vector<double> vetD(n);  // cria vetor com n doubles
+
+	system("pause");
+	return 0;
+}
